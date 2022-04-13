121ea5e2faa0c7aed1bba0892b02d4c5dcc4815b
Raphael Thiago Lemes <raphaellemes@brq.com>
2022-04-13 15:10:43 -0300

    primeira implementacao de monolito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'teste-monolito';
+  pokemons = <any>[]
+  private urlPokemon = 'https://pokeapi.co/api/v2/pokemon/';
+  private urlFormPokemon = 'https://pokeapi.co/api/v2/pokemon-form'
+
+  constructor(private httpService: HttpClient) { }
+
+  ngOnInit(): void {
+    // Captura dos dados da API onde ira trazer todos pokemons
+    this.retornaListaPokemon().subscribe(response => {
+      const retornoAPI = response['body']['results']
+
+      // Captura da API onde ira trazer os dados de cada pokemon retornado e criar um novo array pra armazenar pokemon e dados do pokemon
+      for (let i = 0; i < retornoAPI.length; i++) {
+        this.retornaPokemon(retornoAPI[i].name).subscribe(response => {
+          this.pokemons.push(response['body'])
+
+          // ordenando os pokemons para garantir que o id 1 é referente ao primeiro pokemon retornado na primeira lista
+          this.pokemons.sort(function (a: any, b: any) {
+            return a.id - b.id
+          });
+        });
+      }
+    });
+  }
+
+  retornaListaPokemon() {
+    return this.get<any>(this.urlPokemon);
+  }
+
+  retornaPokemon(id: string) {
+    return this.get<any>(`${this.urlFormPokemon}/${id}`)
+  }
+
+  retornaHeader() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      observe: 'response' as 'response',
+    };
+    return httpOptions;
+  }
+
+  get<T>(url: string): Observable<HttpResponse<T>> {
+    return this.httpService.get<T>(url, this.retornaHeader());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatSliderModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
